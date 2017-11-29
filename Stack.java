b93c8c54d2d69c06f03f76eff0c5ce4931eda0a8
ausminot <31025026+ausminot@users.noreply.github.com>
2017-11-29 09:31:07 -0500

    Not done

--- Stack.java
+++ Stack.java
@@ -1,20 +1,64 @@
 public class Stack<E>
 {
-	public Stack()
-	{}
+    private Object top;
+    private Object[] stack;
+    private int size;
 
-	public void push(E item)
-	{}
+    public Stack()
+    {
+        stack = new Object[10];
+        size = 0;
+    }
 
-	public E pop()
-	{return null;}
+    public void push(E item)
+    {
+        if (size < stack.length && stack[size+1] == null)
+        {
+            stack[size+1] = item;
+            top = stack[size+1];
+        }
+        else
+        {
+            int i = 0;
+            Object[] newStack = new Object[stack.length*2];
+            for (int x = 0; x < size; x++)
+            {
+                newStack[x] = stack[x];
+                i++;
+            }
+            stack = newStack;
+            stack[size+1] = item;
+            size++;
+            top = stack[size-1];
+        }
+    }
 
-	public E peek()
-	{return null;}
+    public E pop() 
+    {
 
-	public boolean isEmpty()
-	{return false;}
+        return null;
+    }
 
-	public int size()
-	{return -1;}
+    public E peek()
+    {
+        return null;
+    }
+
+    public boolean isEmpty()
+    {
+        for (int i = 0; i < stack.length; i++)
+        {
+            if(stack[i] != null)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public int size()
+    {
+        return size;
+    }
 }
